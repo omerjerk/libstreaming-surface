94bcd9f8e473fa48eb0c3e75b826422f22b30bdc
Umair Khan <omerjerk@gmail.com>
2017-04-24 22:25:30 +0530

    initial stuff partially working, surface null
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/src/main/java/com/display/solution/screenshare/MainActivity.java
+++ app/src/main/java/com/display/solution/screenshare/MainActivity.java
@@ -1,13 +1,60 @@
 package com.display.solution.screenshare;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.hardware.display.VirtualDisplay;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static MediaProjection mMediaProjection;
+    private MediaProjectionManager mMediaProjectionManager;
+
+    private static final int REQUEST_MEDIA_PROJECTION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMediaProjectionManager = (MediaProjectionManager)
+                getSystemService(MEDIA_PROJECTION_SERVICE);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            if (resultCode != RESULT_OK) {
+                Toast.makeText(this, "User cancelled the access", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mMediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+            Intent startServerIntent = new Intent(MainActivity.this, ScreenCaptureService.class);
+            startServerIntent.setAction("START");
+            startService(startServerIntent);
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private void startScreenCapture() {
+        startActivityForResult(
+                mMediaProjectionManager.createScreenCaptureIntent(),
+                REQUEST_MEDIA_PROJECTION);
+    }
+
+    @SuppressLint("NewApi")
+    public void start(View v) {
+        startActivityForResult(
+                mMediaProjectionManager.createScreenCaptureIntent(),
+                REQUEST_MEDIA_PROJECTION);
     }
 }
---
+++ app/src/main/java/com/display/solution/screenshare/ScreenCaptureService.java
@@ -0,0 +1,90 @@
+package com.display.solution.screenshare;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.os.IBinder;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+
+import net.majorkernelpanic.streaming.Session;
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.audio.AudioQuality;
+import net.majorkernelpanic.streaming.video.VideoQuality;
+
+public class ScreenCaptureService extends Service {
+
+    private static final String TAG = "ScreenCaptureService";
+
+    private VirtualDisplay virtualDisplay;
+
+    public static final int WIDTH = 1080 / 4;
+    public static final int HEIGHT = 1920 / 4;
+
+    static int deviceWidth;
+    static int deviceHeight;
+    Point resolution = new Point();
+    final float resolutionRatio = 0.25f;
+
+    private Session mSession;
+
+    public ScreenCaptureService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        calculateScreenDimens();
+
+        mSession = SessionBuilder.getInstance()
+//                .setCallback(this)
+                .setPreviewOrientation(90)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setAudioQuality(new AudioQuality(16000, 32000))
+                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                .setVideoQuality(new VideoQuality(320,240,20,500000))
+                .setDestination("localhost")
+                .build();
+
+        mSession.configure();
+
+        createVirtualDisplay();
+
+        Log.d(TAG, mSession.getSessionDescription());
+        mSession.start();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void calculateScreenDimens() {
+        DisplayMetrics dm = new DisplayMetrics();
+        Display mDisplay = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        mDisplay.getMetrics(dm);
+        deviceWidth = dm.widthPixels;
+        deviceHeight = dm.heightPixels;
+        mDisplay.getRealSize(resolution);
+        resolution.x = (int) (resolution.x * resolutionRatio);
+        resolution.y = (int) (resolution.y * resolutionRatio);
+    }
+
+    @SuppressLint("NewApi")
+    public void createVirtualDisplay() {
+        DisplayManager mDisplayManager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
+        virtualDisplay = MainActivity.mMediaProjection.createVirtualDisplay("Remote Droid",
+                WIDTH, HEIGHT, 50,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mSession.getSurface(), null, null);
+    }
+}
